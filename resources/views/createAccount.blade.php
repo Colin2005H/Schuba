e8c28ae4ec402de4bda1af48e9979d0f29109b13
Maelys <joret.maelys@gmail.com>
2025-01-07 17:11:22 +0100

    better css

--- resources/views/createAccount.blade.php
+++ resources/views/createAccount.blade.php
@@ -4,46 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Créer un compte</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" 
-    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="bg-gray-100">
     @if(session('success'))
         <div class="alert alert-success">
             {{session('success')}}
         </div>
     @endif
-    <div class="px-4 py-5 my-5 text-center" style="--bs-bg-opacity: .5; ">
-        <h3>Créer un compte utilisateur</h3>
+    <div class="bg-white p-6 rounded-lg shadow-md sm:w-96 items-center justify-center mx-auto my-auto">
+        
+        <h3 class="text-xl font-semibold text-center mb-6">Créer un compte utilisateur</h3>
+        
         <form method="POST" action="" style="margin-bottom: 10px;">
             @csrf
-            <div class="form-group">
-                <label for="uti_nom">Nom</label>
-                <input type="text" id="uti_nom" name="uti_nom" value="" placeholder="Marie">
+            <div class="mb-4">
+                <label class="flex items-center gap-2 text-gray-700">
+                    <input type="text" id="uti_nom" name="uti_nom" value="" placeholder="Nom" class="w-full px-3 py-2 bg-white border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
+                </label>
                 @error("uti_nom")
                     {{$message}}
                 @enderror
             </div>
 
-            <div class="form-group">
-                <label for="uti_prenom">Prénom</label>
-                <input type="text" id="uti_prenom" name="uti_prenom" value="Gérard">
+            <div class="mb-4">
+                <label class="flex items-center gap-2 text-gray-700">
+                    <input type="text" id="uti_prenom" name="uti_prenom" value="" placeholder="Prénom" class="w-full px-3 py-2 bg-white border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
+                </label>
                 @error("uti_prenom")
                     {{$message}}
                 @enderror
-            </div class="form-group">
+            </div>
 
-            <div class="form-group">
-                <label for="uti_mail">Email</label>
-                <input type="text" id="uti_mail" name="uti_mail" value="gerard.marie@gmail.com">
+            <div class="mb-4">
+                <label class="flex items-center gap-2 text-gray-700">
+                    <input type="text" id="uti_mail" name="uti_mail" value="" placeholder="Adresse email" class="w-full px-3 py-2 bg-white border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
+                </label>
                 @error("uti_mail")
                     {{$message}}
                 @enderror
-            </div class="form-group">
+            </div>
 
-            <div class="form-group">
-                <label for="clu_id">Club</label>
-                <select id="clu_id" name="clu_id">
+            <div class="mb-4">
+                <label for="clu_id" class="flex items-center gap-2 text-gray-700">Club : </label>
+                <select id="clu_id" name="clu_id" class="w-full px-3 py-2 bg-white border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
                     @foreach ($clubs as $club)
                         <option value="{{ $club->clu_id }}">{{ $club->clu_nom }}</option>
                     @endforeach
@@ -52,25 +56,26 @@
                 @error("clu_id")
                     {{$message}}
                 @enderror
-            </div class="form-group">
+            </div>
 
-            <div class="form-group">
-                <label for="niveauUser">Niveau </label>
-                <input id="niveauUser" type="text" name="uti_niveau" value="1">
+            <div class="mb-4">
+                <label class="flex items-center gap-2 text-gray-700">
+                    <input id="niveauUser" type="text" name="uti_niveau" value="" placeholder="Niveau de plongée" class="w-full px-3 py-2 bg-white border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
+                </label>
                 @error("uti_niveau")
                     {{$message}}
                 @enderror
             </div>
 
-            <div class="form-group">
-                <label for="uti_date_naissance">Date de naissance</label>
-                <input id="uti_date_naissance" type="date" name="uti_date_naissance" value="1970-01-01" min="1970-01-01" max="{{today()}}"/>
+            <div class="mb-4">
+                <label for="uti_date_naissance">Date de naissance : </label>
+                <input id="uti_date_naissance" type="date" name="uti_date_naissance" value="1970-01-01" min="1970-01-01" max="{{today()}}" class="w-full px-3 py-2 bg-white border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"/>
                 @error("uti_date_naissance")
                     {{$message}}
                 @enderror
             </div>
 
-            <div class="form-group">
+            <div class="mb-4">
                 <div class="form-check">
                     <input type="radio" id="userType1" name="userType" value="eleve" />
                     <label for="userType1">Eleve</label>
@@ -82,23 +87,23 @@
                 </div>
             </div>
 
-            <button class="btn btn-primary">Enregistrer</button>
+            <button class="flex items-center justify-center text-white hover:bg-blue-500 p-4 rounded-md" style="background-color: rgba(23, 34, 49, 1);">Enregistrer</button>
         </form>
     </div>
 
-        <script>
-            var niveau = document.getElementById("niveauUser");
-            var initiateur = document.getElementById("userType2");
-            initiateur.disabled = true;
+    <script>
+        var niveau = document.getElementById("niveauUser");
+        var initiateur = document.getElementById("userType2");
+        initiateur.disabled = true;
 
-            niveau.addEventListener('input', function () {
-                if(parseInt(niveau.value) < 2){
-                    initiateur.disabled = true;
-                }
-                else{
-                    initiateur.disabled = false;
-                }
-            });
-        </script>
-    </body>
+        niveau.addEventListener('input', function () {
+            if(parseInt(niveau.value) < 2){
+                initiateur.disabled = true;
+            }
+            else{
+                initiateur.disabled = false;
+            }
+        });
+    </script>
+</body>
 </html>
