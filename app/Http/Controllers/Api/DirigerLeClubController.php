66c5cb2c53a6293ac2a067f6de14292d0fdc8099
brault231 <brault231@etu.unicaen.fr>
2025-01-07 15:22:18 +0100

    calendrier parfait

--- app/Http/Controllers/Api/DirigerLeClubController.php
+++ app/Http/Controllers/Api/DirigerLeClubController.php
@@ -6,11 +6,29 @@ use App\Http\Controllers\Controller;
 use App\Models\DirigerLeClub;
 use Illuminate\Http\Request;
 
+use Vyuldashev\LaravelOpenApi\Attributes as OpenApi;
+
+#[OpenApi\PathItem]
 class DirigerLeClubController extends Controller {
+    
+    /**
+     * dirigeants
+     *
+     * Liste les dirigeants de club
+     */
+    #[OpenApi\Operation]
     function liste(){
         return response()->json(DirigerLeClub::all());
     }
 
+    /**
+     * dirigeants
+     *
+     * Liste les dirigeants de club
+     * 
+     * @param Int $id identifiant du dirigeant
+     */
+    #[OpenApi\Operation]
     function detail($id){
         return response()->json(DirigerLeClub::find($id));
     }
--- app/Http/Controllers/Api/PloSeanceController.php
+++ app/Http/Controllers/Api/PloSeanceController.php
@@ -2,13 +2,41 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use App\Models\PloSeance;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PloSeanceController extends Controller {
-    function liste(){
-        return response()->json(PloSeance::all());
+    public function liste(Request $request){
+        $dateDebut = $request->input('start');
+        $dateFin = $request->input('end');
+
+        $query = PloSeance::query();
+
+        if ($dateDebut) {
+            $dateDebut = Carbon::parse($dateDebut);
+            $query->where('SEA_DATE_DEB', '>=', $dateDebut);
+        }
+
+        if ($dateFin) {
+            $dateFin = Carbon::parse($dateFin);
+            $query->where('SEA_DATE_FIN', '<=', $dateFin);
+        }
+
+        $seances = $query->get();
+
+        // Transformer les séances pour le format FullCalendar
+        $events = $seances->map(function ($seance) {
+            return [
+                'id' => $seance->SEA_ID,
+                'niveau' =>$seance->FORM_NIVEAU,
+                'start' => $seance->SEA_DATE_DEB->toIso8601String(),
+                'end' => $seance->SEA_DATE_FIN->toIso8601String()
+            ];
+        });
+
+        return response()->json($events);
     }
 
     function detail($id){
