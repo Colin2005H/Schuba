ccf88a6f219afd29c71d92908d8a23565fd8f9e1
Leo Berranger <lele.berranger@gmail.com>
2025-01-07 09:34:45 +0100

    fix: ajout du prefix

--- app/Http/Controllers/SeanceController.php
+++ app/Http/Controllers/SeanceController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PloSeance;
+use App\Models\Seance;
 use Illuminate\Http\Request;
 
 class SeanceController extends Controller
 {
     public function index(){
         echo "ça marche tkt";
-        return view('creer-seance');
+        return view('creer-seance', ['seance' => PloSeance::all()]);
     } 
 }
--- app/Models/PloSeance.php
+++ app/Models/PloSeance.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Seance extends Model
+class PloSeance extends Model
 {
     use HasFactory;
 }
