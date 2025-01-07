9557965f2c9c76991872ed1533086ecc593baaa1
Maelys <joret.maelys@gmail.com>
2025-01-07 16:50:35 +0100

    advance in style

--- resources/views/createAccount.blade.php
+++ resources/views/createAccount.blade.php
@@ -13,12 +13,13 @@
             {{session('success')}}
         </div>
     @endif
-    <h3>Créer un compte utilisateur</h3>
-        <form method="POST" action="">
+    <div class="px-4 py-5 my-5 text-center" style="--bs-bg-opacity: .5; ">
+        <h3>Créer un compte utilisateur</h3>
+        <form method="POST" action="" style="margin-bottom: 10px;">
             @csrf
             <div class="form-group">
                 <label for="uti_nom">Nom</label>
-                <input type="text" id="uti_nom" name="uti_nom" value="">
+                <input type="text" id="uti_nom" name="uti_nom" value="" placeholder="Marie">
                 @error("uti_nom")
                     {{$message}}
                 @enderror
@@ -26,7 +27,7 @@
 
             <div class="form-group">
                 <label for="uti_prenom">Prénom</label>
-                <input type="text" id="uti_prenom" name="uti_prenom" value="">
+                <input type="text" id="uti_prenom" name="uti_prenom" value="Gérard">
                 @error("uti_prenom")
                     {{$message}}
                 @enderror
@@ -34,7 +35,7 @@
 
             <div class="form-group">
                 <label for="uti_mail">Email</label>
-                <input type="text" id="uti_mail" name="uti_mail" value="">
+                <input type="text" id="uti_mail" name="uti_mail" value="gerard.marie@gmail.com">
                 @error("uti_mail")
                     {{$message}}
                 @enderror
@@ -55,7 +56,7 @@
 
             <div class="form-group">
                 <label for="niveauUser">Niveau </label>
-                <input id="niveauUser" type="text" name="uti_niveau" value="0">
+                <input id="niveauUser" type="text" name="uti_niveau" value="1">
                 @error("uti_niveau")
                     {{$message}}
                 @enderror
@@ -83,6 +84,7 @@
 
             <button class="btn btn-primary">Enregistrer</button>
         </form>
+    </div>
 
         <script>
             var niveau = document.getElementById("niveauUser");
