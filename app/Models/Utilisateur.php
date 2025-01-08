7aea0928d69a51a6b297270ba9353237b60dc223
Maelys <joret.maelys@gmail.com>
2025-01-08 09:13:08 +0100

    merge branch 'main' into branch 'Maelys'

--- app/Models/Utilisateur.php
+++ app/Models/Utilisateur.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Utilisateur extends Model
 {
+    use HasFactory;
+    
     protected $fillable = ['clu_id', 'uti_id','uti_nom','uti_prenom', 'uti_mail', 'uti_mdp', 'uti_niveau', 'uti_date_naissance', 'uti_date_creation', 
                            'uti_date_certificat', 'uti_code_postal', 'uti_adresse', 'uti_ville'];
 
-    use HasFactory;
     protected $table = 'plo_utilisateur';
     public $timestamps = false;
     protected $primaryKey = 'uti_id';
