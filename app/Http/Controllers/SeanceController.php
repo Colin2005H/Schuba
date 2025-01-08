83d354d3de1b1654f55d3d52a2da0234c6b4164a
Leo Berranger <lele.berranger@gmail.com>
2025-01-08 11:01:49 +0100

    possibilité d'avoir plusieurs initiateurs dans une séance

--- app/Http/Controllers/SeanceController.php
+++ app/Http/Controllers/SeanceController.php
@@ -19,23 +19,24 @@ class SeanceController extends Controller
      *
      * @return void
      */
-    public function createSession(){
+    public function createSession()
+    {
         return view('creer-seance', [
             'lieux' => Lieu::all(),
 
-            'aptitudes'=>Aptitude::all(),
+            'aptitudes' => Aptitude::all(),
 
-            'initiateurs'=>Initiator::all()->map(function(Initiator $initiator){
-                    return $initiator->user()->getResults();
-                })->filter(function ($user, $key) {
-                    return $user != null;
-                }), 
+            'initiateurs' => Initiator::all()->map(function (Initiator $initiator) { //prends tous les Initiator
+                return $initiator->user()->getResults();
+            })->filter(function ($user, $key) {
+                return $user != null;
+            }),
 
-            'eleves'=>Eleve::all()->map(function(Eleve $student){
-                    return $student->user()->getResults();
-                })->filter(function ($user, $key) {
-                    return $user != null;
-                })
+            'eleves' => Eleve::all()->map(function (Eleve $student) { //prends tous les eleves
+                return $student->user()->getResults();
+            })->filter(function ($user, $key) {
+                return $user != null;
+            })
         ]);
     }
 
@@ -45,38 +46,55 @@ class SeanceController extends Controller
      * @param Request $request
      * @return void
      */
-    public function store(Request $request){
-        
-        
-        try{
-            $startTime = $request->input('date')."T".$request->input('beginHour').":".$request->input('beginMin');
-            $endTime = $request->input('date')."T".$request->input('endHour').":".$request->input('endMin');
+    public function store(Request $request)
+    {
+
+
+        try {
+            $startTime = $request->input('date') . "T" . $request->input('beginHour') . ":" . $request->input('beginMin');
+            $endTime = $request->input('date') . "T" . $request->input('endHour') . ":" . $request->input('endMin');
 
             $sessionId = Seance::insert($startTime, $endTime, $request->input('lieu'), $request->input('niv'));
-            
-            $group = Groupe::create([
-                "SEA_ID" => $sessionId,
-                "UTI_ID_INITIATEUR" => $request->input('initiateur'),
-                "UTI_ID"=>$request->input('eleve1'),
-                "GRP_PRESENCE"=>NULL
-            ]);
 
-
-            if($request->input('eleve2') !=="null"){
-                $group = Groupe::create([
-                    "SEA_ID" => $sessionId,
-                    "UTI_ID_INITIATEUR" => $request->input('initiateur'),
-                    "UTI_ID"=>$request->input('eleve2'),
-                    "GRP_PRESENCE"=> NULL
-                ]);
+            foreach ($request->input('group') as $group) {
+                $this->createGroup($group, $sessionId);
             }
 
-        }catch(Exception $e){
+            
+        } catch (Exception $e) {
             return redirect()->route('createSession.show')->with('success', $e->getMessage());
         }
-        
+
         $request->input('aptitude');
 
         return redirect()->route('createSession.show')->with('success', "La séance a été créée");
+    }
+
+    /**
+     * Créé un groupe avec le tableau en parametre
+     *
+     * @param array<string, string> $group tableau avec des clés "initiateur", "eleve1" et "eleve2"
+     * @param int $sessionId id de la session dont le groupe fait parti
+     * @return void
+     */
+    public function createGroup($group, $sessionId)
+    {
+        print_r($group);
+        Groupe::create([
+            "SEA_ID" => $sessionId,
+            "UTI_ID_INITIATEUR" => $group["initiateur"],
+            "UTI_ID" => $group["eleve1"],
+            "GRP_PRESENCE" => NULL
+        ]);
+
+
+        if ($group["eleve2"] !== "null") {
+            Groupe::create([
+                "SEA_ID" => $sessionId,
+                "UTI_ID_INITIATEUR" => $group["initiateur"],
+                "UTI_ID" => $group["eleve2"],
+                "GRP_PRESENCE" => NULL
+            ]);
+        }
     }
 }
--- resources/views/creer-seance.blade.php
+++ resources/views/creer-seance.blade.php
@@ -7,6 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Création d'une session</title>
+        <script defer src="{{ asset('js/createSession.js') }}"></script>
     </head>
     <body>
         @if (session('success'))
@@ -43,41 +44,53 @@
             <input type="date" id="date" name="date" required><br>
 
             <label for="beginHour">Heure de début :</label><br>
-            <div><input type="number" id="beginHour" name="beginHour" required min="0" max="23" value="0">:
-                <input type="number" id="beginMin" name="beginMin" required min="0" max="59" value="0"></div><br><br>
-
+            <div>
+                <input type="number" id="beginHour" name="beginHour" required min="0" max="23" value="0">:
+                <input type="number" id="beginMin" name="beginMin" required min="0" max="59" value="0">
+            </div><br>
 
             <label for="endHour">Heure de fin :</label><br>
-            <div><input type="number" id="endHour" name="endHour" required min="0" max="23" value="0">:
-                <input type="number" id="endMin" name="endMin" required min="0" max="59" value="0"></div><br>
+            <div>
+                <input type="number" id="endHour" name="endHour" required min="0" max="23" value="0">:
+                <input type="number" id="endMin" name="endMin" required min="0" max="59" value="0">
+            </div><br>
 
-            <label for="initiateur">Initiateur : </label>
-            <select name="initiateur" id="initiateur">
+            <div id="groupGlobalContainer">
+                <div id="groupContainer1">
+                    <label for="initiateur">Initiateur : </label>
+                    <select name="group[1][initiateur]" id="initiateur">
+                        
+                        @foreach($initiateurs as $init)
+                            <option value="{{$init->UTI_ID}}">{{$init->UTI_NOM}} {{$init->UTI_PRENOM}}</option>
+                        @endforeach
+
+                    </select>
+
+                    <label for="eleve1">Eleve 1 : </label>
+                    <select name="group[1][eleve1]" id="eleve1" required>
+                        
+                        @foreach($eleves as $eleve)
+                            <option value="{{$eleve->UTI_ID}}">{{$eleve->UTI_NOM}} {{$eleve->UTI_PRENOM}}</option>
+                        @endforeach
+
+                    </select>
+
+                    <label for="eleve2">Eleve 2 : </label>
+                    <select name="group[1][eleve2]" id="eleve2">
+                        <option value="null">Aucun</option>
+
+                        @foreach($eleves as $eleve)
+                            <option value="{{$eleve->UTI_ID}}">{{$eleve->UTI_NOM}} {{$eleve->UTI_PRENOM}}</option>
+                        @endforeach
+
+                    </select>
+                </div>
                 
-                @foreach($initiateurs as $init)
-                    <option value="{{$init->UTI_ID}}">{{$init->UTI_NOM}} {{$init->UTI_PRENOM}}</option>
-                @endforeach
+            </div>
 
-            </select>
-
-            <label for="eleve1">Eleve 1 : </label>
-            <select name="eleve1" id="eleve1" required>
+            <button type="button" id="plus" onclick="addGroupInput()">+</button>
+            <button type="button" id="minus" onclick="removeGroupInput()">-</button>
                 
-                @foreach($eleves as $eleve)
-                    <option value="{{$eleve->UTI_ID}}">{{$eleve->UTI_NOM}} {{$eleve->UTI_PRENOM}}</option>
-                @endforeach
-
-            </select>
-
-            <label for="eleve2">Eleve 2 : </label>
-            <select name="eleve2" id="eleve2">
-                <option value="null">Aucun</option>
-
-                @foreach($eleves as $eleve)
-                    <option value="{{$eleve->UTI_ID}}">{{$eleve->UTI_NOM}} {{$eleve->UTI_PRENOM}}</option>
-                @endforeach
-
-            </select>
             <br>
             <input type="submit" value="Créer la séance">
             
