21982d9290c3df90f027f11a5de12bdc9396dab2
Colin Houlette <153432267+Colin2005H@users.noreply.github.com>
2025-01-09 22:28:19 +0100

    Comment in ListUserController ModifierCompteController and in their views

--- app/Http/Controllers/ListUserController.php
+++ app/Http/Controllers/ListUserController.php
@@ -4,23 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+//listUserController is a controller class that handles the display of the list of users
 class listUserController extends Controller
 {
+    //showListUser function that displays the list of users
     public function showListUser(){
-        $liste_eleve = DB::table('PLO_UTILISATEUR')
-        ->where('UTI_SUPPRIME',0)
-        ->get();
+        $liste_eleve = DB::table('PLO_UTILISATEUR') //fetch all users
+        ->where('UTI_SUPPRIME',0) //filter users that are not deleted
+        ->get(); //get the result
         
-        return view('listUser')->with(compact('liste_eleve'));
+        return view('listUser')->with(compact('liste_eleve')); //return the view with the list of users
     }
-
+    //deleteUser function that deletes a user
     public function deleteUser(Request $request){
-        $id = $request->input('action');
-        DB::table('PLO_UTILISATEUR')
+        $id = $request->input('action'); //get the id of the user to delete
+        DB::table('PLO_UTILISATEUR')//delete the user
                 ->where('UTI_ID', $id)
                 ->update([
-                    'UTI_SUPPRIME' => 1,
+                    'UTI_SUPPRIME' => 1,//set the user as deleted
         ]);
         return redirect()->back()->with('success', "L'utilisateur a bien été supprimé");
     }
--- app/Http/Controllers/ModifierCompteController.php
+++ app/Http/Controllers/ModifierCompteController.php
@@ -8,33 +8,34 @@ use Illuminate\Support\Facades\DB;
 // ModifierCompteController is a controller class that handles the modification of a user account
 class ModifierCompteController extends Controller
 {
-
+    // function that displays the modification form
     public function edit($id)
     {
-        $info_compte = DB::table('PLO_UTILISATEUR')
-        ->where('UTI_Id',$id)
+        $info_compte = DB::table('PLO_UTILISATEUR')//fetch the user information
+        ->where('UTI_Id',$id)//filter the user by id
         ->get();
         $club = DB::table('PLO_CLUB')->where('CLU_ID', '=', $info_compte[0]->CLU_ID)->get();
         /*
         $director = DB::table('diriger_le_club')->select('clu_id')->where('uti_id', '=', session('user')->UTI_ID)->get();
         $clubDirector = DB::table('plo_club')->select('clu_id', 'clu_nom')->where('clu_id', '=', $director[0]->clu_id)->get();*/
-        return view('modifierCompte', compact('info_compte','club')); 
+        return view('modifierCompte', compact('info_compte','club')); //return the view with the user information and the club 
     }
+    // function that updates the user information
     public function update(Request $request, $id){
 
-        if ($request->input('action') == 'Annuler'){
-            return redirect()->route('listUser');
+        if ($request->input('action') == 'Annuler'){   //if the user clicks on the cancel button
+            return redirect()->route('listUser'); //redirect to the list of users
         }
         else{
 
-            $info_compte = DB::table('PLO_UTILISATEUR')
+            $info_compte = DB::table('PLO_UTILISATEUR') 
             ->where('UTI_Id',$id)
             ->get();
 
-            $club = DB::table('PLO_CLUB')->where('CLU_ID', '=', $info_compte[0]->CLU_ID)->get();
+            $club = DB::table('PLO_CLUB')->where('CLU_ID', '=', $info_compte[0]->CLU_ID)->get(); //fetch the club information of the user
 
-            if($request->input('uti_mail') != $info_compte[0]->UTI_MAIL){
-                $this->validate($request, [
+            if($request->input('uti_mail') != $info_compte[0]->UTI_MAIL){ //if the user changes his email
+                $this->validate($request, [ 
                     'uti_mail' => 'bail|required|unique:plo_utilisateur|email',
                 ], [
                     'uti_mail.email' => "Le texte doit correspondre à une adresse email valide",
@@ -42,7 +43,7 @@ class ModifierCompteController extends Controller
                     'uti_mail.required' => "Le champ doit être rempli",
                 ]);
             }
-            $this->validate($request, [
+            $this->validate($request, [ //validate the user information
                 'uti_mail' => 'bail|required|email',
                 'uti_nom' => 'bail|required',
                 'uti_prenom' => 'bail|required',
@@ -52,7 +53,7 @@ class ModifierCompteController extends Controller
                 'uti_niveau' => 'bail|required',
                 'uti_date_naissance' => 'bail|required',
                 'uti_date_certificat' => 'bail|required'
-            ], [
+            ], [  //error messages
                 'uti_mail.email' => "Le texte doit correspondre à une adresse email valide",
                 'uti_mail.required' => "Le champ doit être rempli",
 
@@ -72,9 +73,9 @@ class ModifierCompteController extends Controller
                 'uti_date_naissance.required' => "Le champ doit être rempli"
             ]);
 
-            DB::table('PLO_UTILISATEUR')
+            DB::table('PLO_UTILISATEUR') //update the user information
                 ->where('UTI_ID', $id)
-                ->update([
+                ->update([ 
                     'CLU_ID' => $club[0]->CLU_ID,
                     'UTI_NOM' => $request->input('uti_nom'),
                     'UTI_PRENOM' => $request->input('uti_prenom'),
@@ -84,10 +85,10 @@ class ModifierCompteController extends Controller
                     'UTI_VILLE' => $request->input('uti_ville'),
                     'UTI_NIVEAU' => $request->input('uti_niveau'),
                     'UTI_DATE_CERTIFICAT' => $request->input('uti_date_certificat'),
-                    'UTI_DATE_NAISSANCE' => $request->input('uti_date_naissance'),
+                    'UTI_DATE_NAISSANCE' => $request->input('uti_date_naissance'), 
             ]);
 
-            switch($request->input('userType')){
+            switch($request->input('userType')){ //update the user type
                 case 'eleve':
                     $sql = DB::table('PLO_INITIATEUR')->where('UTI_ID',$id)->count();
                     if($sql==1){
@@ -112,7 +113,7 @@ class ModifierCompteController extends Controller
                     break;
             }
 
-            return redirect()->route('listUser')->with('success', "L'utilisateur a bien été modifié");
+            return redirect()->route('listUser')->with('success', "L'utilisateur a bien été modifié"); //redirect to the list of users with a success message 
 
         }
     }
