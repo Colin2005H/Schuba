9d36ce51901ec4bda300c5f23fababc060d4e6bc
KDual Louane <kdual231@unicaen.fr>
2025-01-10 11:32:51 +0100

    fin modif louane

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,12 +2,13 @@
     <script src="https://cdn.tailwindcss.com"></script>
 
     <header class="bg-white text-black shadow drop-shadow-lg">
-        <div class="container mx-auto flex items-center  py-0 px-3">
+        <div class="container mx-auto flex items-center py-2 px-3">
             <!-- Logo -->
             <a href="{{ url('/home') }}" class="text-lg font-bold">
                 <img src="{{ asset('img/logo1.svg') }}" alt="Logo" class="h-20">
             </a>
             <img src="{{ asset('img/Diver.png') }}" alt="diver" class="mr-3">
+
             @php
                 use App\Http\Controllers\RoleController;
                 use Illuminate\Support\Facades\Auth;
@@ -20,72 +21,104 @@
 
             @endphp
 
-            @if ($role === 'eleve')
-                <!-- Navigation élève -->
-                <nav class="hidden md:flex space-x-6">
+            <!-- Mobile Menu Button -->
+            <button id="mobile-menu-button" class="md:hidden focus:outline-none">
+                <i class="fas fa-bars"></i> <!-- Hamburger Icon -->
+            </button>
+
+            <!-- Desktop Navigation -->
+            <nav class="hidden md:flex space-x-6">
+                @if ($role === 'eleve')
                     <a href="{{ url('/profile') }}" class="hover:text-gray-400">Mon compte</a>
                     <a href="{{ url('/calendar') }}" class="hover:text-gray-400">Séances</a>
                     <a href="{{ url('/aptitudes/' . $userid) }}" class="hover:text-gray-400">Bilan de Compétences</a>
                     <a href="{{ url('/') }}" class="hover:text-gray-400">Se Déconnecter</a>
-                </nav>
-            @elseif($role === 'responsable')
-                <!-- Navigation responsable-->
-                <nav class="hidden md:flex space-x-6">
+                @elseif($role === 'responsable')
                     <a href="{{ url('/profile') }}" class="hover:text-gray-400">Mon compte</a>
                     <a href="{{ url('/calendar') }}" class="hover:text-gray-400">Séances</a>
                     <a href="{{ url('/createSession') }}" class="hover:text-gray-400">Crée Séances</a>
                     <a href="{{ url('/globalAptitudes/' . $teachingLevel) }}" class="hover:text-gray-400">Bilan des
-                        Compétences</a><!-- listeleves-->
+                        Compétences</a>
                     <a href="{{ url('/liste-membres-formation') }}" class="hover:text-gray-400">Membres</a>
                     <a href="{{ url('/') }}" class="hover:text-gray-400">Se Déconnecter</a>
-                </nav>
-            @elseif($role === 'initiateur')
-                <!-- Navigation initiateur-->
-                <nav class="hidden md:flex space-x-6">
+                @elseif($role === 'initiateur')
                     <a href="{{ url('/profile') }}" class="hover:text-gray-400">Mon compte</a>
                     <a href="{{ url('/calendar') }}" class="hover:text-gray-400">Séances</a>
                     <a href="{{ url('/globalAptitudes/' . $teachingLevel) }}" class="hover:text-gray-400">Bilan des
-                        Compétences</a><!-- listeleves-->
+                        Compétences</a>
                     <a href="{{ url('/') }}" class="hover:text-gray-400">Se Déconnecter</a>
-                </nav>
-            @elseif($role === 'directeur_technique')
-                <!-- Navigation directeur technique-->
-                <nav class="hidden md:flex space-x-6">
+                @elseif($role === 'directeur_technique')
                     <a href="{{ url('/profile') }}" class="hover:text-gray-400">Mon compte</a>
                     <a href="{{ url('/formations') }}" class="hover:text-gray-400">Formations</a>
-                    <a href="{{ url('/listUser') }}" class="hover:text-gray-400">Gestions des
-                        Utilisateurs</a><!-- listUser-->
-                    <a href="{{ url('/createAccount') }}" class="hover:text-gray-400">Créer
-                        Comptes</a><!-- Create account-->
-                    <a href="{{ url('/choisir') }}" class="hover:text-gray-400">Bilan des
-                        Compétences</a><!-- PAGE CHOISIR LE NIVEAU-->
+                    <a href="{{ url('/listUser') }}" class="hover:text-gray-400">Gestions des Utilisateurs</a>
+                    <a href="{{ url('/createAccount') }}" class="hover:text-gray-400">Créer Comptes</a>
+                    <a href="{{ url('/choisir') }}" class="hover:text-gray-400">Bilan des Compétences</a>
                     <a href="{{ url('/manageAptitudes') }}" class="hover:text-gray-400">Aptitudes</a>
                     <a href="{{ url('/') }}" class="hover:text-gray-400">Se Déconnecter</a>
+                @endif
+            </nav>
+
+            <!-- Mobile Navigation (Hidden by default) -->
+            <div class="md:hidden" id="mobile-menu"
+                class="hidden bg-gray-700 absolute top-0 left-0 w-full h-full z-50 bg-opacity-90">
+                <nav class="flex flex-col p-4 bg-gray-50">
+                    @if ($role === 'eleve')
+                        <a href="{{ url('/profile') }}" class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Mon
+                            compte</a>
+                        <a href="{{ url('/calendar') }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Séances</a>
+                        <a href="{{ url('/aptitudes/' . $userid) }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Bilan de Compétences</a>
+                        <a href="{{ url('/') }}" class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Se
+                            Déconnecter</a>
+                    @elseif($role === 'responsable')
+                        <a href="{{ url('/profile') }}" class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Mon
+                            compte</a>
+                        <a href="{{ url('/calendar') }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Séances</a>
+                        <a href="{{ url('/createSession') }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Crée Séances</a>
+                        <a href="{{ url('/globalAptitudes/' . $teachingLevel) }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Bilan des Compétences</a>
+                        <a href="{{ url('/liste-membres-formation') }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Membres</a>
+                        <a href="{{ url('/') }}" class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Se
+                            Déconnecter</a>
+                    @elseif($role === 'initiateur')
+                        <a href="{{ url('/profile') }}" class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Mon
+                            compte</a>
+                        <a href="{{ url('/calendar') }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Séances</a>
+                        <a href="{{ url('/globalAptitudes/' . $teachingLevel) }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Bilan des Compétences</a>
+                        <a href="{{ url('/') }}" class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Se
+                            Déconnecter</a>
+                    @elseif($role === 'directeur_technique')
+                        <a href="{{ url('/profile') }}" class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Mon
+                            compte</a>
+                        <a href="{{ url('/formations') }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Formations</a>
+                        <a href="{{ url('/listUser') }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Gestions des Utilisateurs</a>
+                        <a href="{{ url('/createAccount') }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Créer Comptes</a>
+                        <a href="{{ url('/choisir') }}" class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Bilan
+                            des Compétences</a>
+                        <a href="{{ url('/manageAptitudes') }}"
+                            class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Aptitudes</a>
+                        <a href="{{ url('/') }}" class="block py-2 px-3 text-gray-900 hover:bg-gray-100">Se
+                            Déconnecter</a>
+                    @endif
                 </nav>
-            @endif
-
-            <!-- Mobile Menu Button -->
-            <button id="mobile-menu-button" class="md:hidden focus:outline-none">
-                <i class="fas fa-bars"></i>
-            </button>
+            </div>
         </div>
-
-        <!-- Mobile Navigation -->
-        <!-- Mobile Navigation
-    <nav id="mobile-menu" class="md:hidden bg-gray-700 hidden">
-        <a href="{{ url('/') }}" class="block px-4 py-2 hover:bg-gray-600">Accueil</a>
-        <a href="{{ url('/about') }}" class="block px-4 py-2 hover:bg-gray-600">À propos</a>
-        <a href="{{ url('/services') }}" class="block px-4 py-2 hover:bg-gray-600">Services</a>
-        <a href="{{ url('/contact') }}" class="block px-4 py-2 hover:bg-gray-600">Contact</a>
-    </nav>
-     -->
     </header>
-</head>
 
-<script>
-    // Script pour le menu mobile
-    document.getElementById('mobile-menu-button').addEventListener('click', () => {
-        const menu = document.getElementById('mobile-menu');
-        menu.classList.toggle('hidden');
-    });
-</script>
+    <script>
+        // Script pour le menu mobile
+        document.getElementById('mobile-menu-button').addEventListener('click', () => {
+            const menu = document.getElementById('mobile-menu');
+            menu.classList.toggle('hidden'); // Toggle visibility
+        });
+    </script>
+</head>
--- resources/views/listUser.blade.php
+++ resources/views/listUser.blade.php
@@ -14,67 +14,69 @@
 <body class="h-screen bg-gray-100 ">
 
     <!-- Conteneur du tableau centré -->
-    <div class="w-3/4 max-w-5xl scalable overflow-y-auto bg-white shadow-lg rounded-lg  self-center">
-        <table class="min-w-full table-auto">
-            <thead>
-                <tr class="bg-gray-200">
-                    <th class="text-center py-4 px-6">NOM</th>
-                    <th class="text-center py-4 px-6">PRENOM</th>
-                    <th class="text-center py-4 px-6">NIVEAU</th>
-                    <th class="text-center py-4 px-6">DATE DE CERTIFICAT</th>
-                    <th class="text-center py-4 px-6">NIVEAU</th>
-                    <th class="text-center py-4 px-6" colspan="2">GESTION</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($liste_eleve as $item)
-                    @if ($item->UTI_SUPPRIME == 0)
-                        <td class="text-center py-4 px-6">{{ $item->UTI_NOM }}</td>
-                        <td class="text-center py-4 px-6">{{ $item->UTI_PRENOM }}</td>
-                        @switch($item->UTI_NIVEAU)
-                            @case(0)
-                                <td class="text-center py-4 px-6">PAS DE NIVEAU</td>
-                            @break
+    <div class="bg-[url('/img/scub1.png')] h-full bg-cover bg-no-repeat bg-center p-10">
+        <div class="w-3/4 max-w-5xl max-h-96 overflow-y-scroll bg-white shadow-lg rounded-lg justify-self-center">
+            <table class="min-w-full table-auto">
+                <thead>
+                    <tr class="bg-gray-200">
+                        <th class="text-center py-4 px-6">NOM</th>
+                        <th class="text-center py-4 px-6">PRENOM</th>
+                        <th class="text-center py-4 px-6">NIVEAU</th>
+                        <th class="text-center py-4 px-6">DATE DE CERTIFICAT</th>
+                        <th class="text-center py-4 px-6">NIVEAU</th>
+                        <th class="text-center py-4 px-6" colspan="2">GESTION</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($liste_eleve as $item)
+                        @if ($item->UTI_SUPPRIME == 0)
+                            <td class="text-center py-4 px-6">{{ $item->UTI_NOM }}</td>
+                            <td class="text-center py-4 px-6">{{ $item->UTI_PRENOM }}</td>
+                            @switch($item->UTI_NIVEAU)
+                                @case(0)
+                                    <td class="text-center py-4 px-6">PAS DE NIVEAU</td>
+                                @break
 
-                            @case($item->UTI_NIVEAU > 0 && $item->UTI_NIVEAU < 5)
-                                <td class="text-center py-4 px-6">N{{ $item->UTI_NIVEAU }}</td>
-                            @break
+                                @case($item->UTI_NIVEAU > 0 && $item->UTI_NIVEAU < 5)
+                                    <td class="text-center py-4 px-6">N{{ $item->UTI_NIVEAU }}</td>
+                                @break
 
-                            @case(5)
-                                <td class="text-center py-4 px-6">MF1</td>
-                            @break
+                                @case(5)
+                                    <td class="text-center py-4 px-6">MF1</td>
+                                @break
 
-                            @case(6)
-                                <td class="text-center py-4 px-6">MF1</td>
-                            @break
-                        @endswitch
+                                @case(6)
+                                    <td class="text-center py-4 px-6">MF1</td>
+                                @break
+                            @endswitch
 
 
-                        <td class="text-center py-4 px-6">{{ $item->UTI_DATE_CERTIFICAT }}</td>
-                        <td class="text-center py-4 px-6">{{ $item->UTI_MAIL }}</td>
+                            <td class="text-center py-4 px-6">{{ $item->UTI_DATE_CERTIFICAT }}</td>
+                            <td class="text-center py-4 px-6">{{ $item->UTI_MAIL }}</td>
 
-                        <td class="text-center py-4 px-6"><a
-                                href="{{ route('modifierCompte', ['id' => $item->UTI_ID]) }}"
-                                class="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-orange-300">
-                                Modifier
-                            </a>
-                        </td>
+                            <td class="text-center py-4 px-6"><a
+                                    href="{{ route('modifierCompte', ['id' => $item->UTI_ID]) }}"
+                                    class="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-orange-300">
+                                    Modifier
+                                </a>
+                            </td>
 
-                        <form action="/listUser" method="post">
-                            @csrf
-                            <td class="text-center py-4 px-6"><button name="action" value="{{ $item->UTI_ID }}"
-                                    class="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-red-300"
-                                    onclick="submitDelete()" href ="modifier">Supprimer</button></td>
-                        </form>
-                        </tr>
-                    @endif
-                @endforeach
+                            <form action="/listUser" method="post">
+                                @csrf
+                                <td class="text-center py-4 px-6"><button name="action" value="{{ $item->UTI_ID }}"
+                                        class="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-red-300"
+                                        onclick="submitDelete()" href ="modifier">Supprimer</button></td>
+                            </form>
+                            </tr>
+                        @endif
+                    @endforeach
 
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
+        <div id="overlay" class="fixed inset-0 bg-black bg-opacity-50 hidden" onclick="closeAlert()"></div>
     </div>
-    <div id="overlay" class="fixed inset-0 bg-black bg-opacity-50 hidden" onclick="closeAlert()"></div>
-
+    @include('footer')
 
     <script>
         // Simuler l'action de soumission du formulaire
