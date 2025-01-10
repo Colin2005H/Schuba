552d989db8e90a55542ddae1acd374d09cbd7b17
KDual Louane <kdual231@unicaen.fr>
2025-01-10 12:02:36 +0100

    run final

--- resources/views/memberList.blade.php
+++ resources/views/memberList.blade.php
@@ -1,98 +1,110 @@
-@extends('head')
+<!DOCTYPE html>
+<html lang="fr">
 
-@section('title','Liste Membres')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste Menbres</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+@include('header')
 
-@section('content')
+<body>
+    <div class="bg-[url('/img/underwater.png')] h-full bg-cover bg-no-repeat bg-center p-10 justify-items-center">
 
-<h1 class="font-bold text-[#004B85] text-center text-2xl m-4">Liste des élèves</h1>
-<div class="w-3/4 max-w-5xl scalable overflow-y-scroll mb-16 max-h-96 bg-white shadow-lg rounded-lg flex items-center justify-center mx-auto">
-    <table class="min-w-full table-auto">
-      <thead>
-        <tr class="bg-gray-200">
-          <th class="text-center py-4 px-6">NOM</th>
-          <th class="text-center py-4 px-6">PRENOM</th>
-          <th class="text-center py-4 px-6">NIVEAU</th>
-          <th class="text-center py-4 px-6">MAIL</th>
-        </tr>
-      </thead>
-      <tbody>
-         @foreach ($studentList as $item)
-          @if($item->UTI_SUPPRIME==0)
+        <h1 class="font-bold text-[#ffffff] text-center text-2xl m-4">Liste des élèves</h1>
+        <div
+            class="w-3/4 max-w-5xl scalable overflow-y-scroll mb-16 max-h-96 bg-white shadow-lg rounded-lg flex items-center justify-center mx-auto">
+            <table class="min-w-full table-auto">
+                <thead>
+                    <tr class="bg-gray-200">
+                        <th class="text-center py-4 px-6">NOM</th>
+                        <th class="text-center py-4 px-6">PRENOM</th>
+                        <th class="text-center py-4 px-6">NIVEAU</th>
+                        <th class="text-center py-4 px-6">MAIL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($studentList as $item)
+                        @if ($item->UTI_SUPPRIME == 0)
+                            <td class="text-center py-4 px-6">{{ $item->UTI_NOM }}</td>
+                            <td class="text-center py-4 px-6">{{ $item->UTI_PRENOM }}</td>
+                            @switch($item->UTI_NIVEAU)
+                                @case(0)
+                                    <td class="text-center py-4 px-6">PAS DE NIVEAU</td>
+                                @break
 
-                  <td class="text-center py-4 px-6">{{$item->UTI_NOM}}</td>
-                  <td class="text-center py-4 px-6">{{$item->UTI_PRENOM}}</td>
-                  @switch($item->UTI_NIVEAU)
-                      @case(0)
-                          <td class="text-center py-4 px-6">PAS DE NIVEAU</td>
-                          @break
-                      @case($item->UTI_NIVEAU > 0 && $item->UTI_NIVEAU < 5 )
-                          <td class="text-center py-4 px-6">N{{$item->UTI_NIVEAU}}</td>
-                          @break
-                      @case(5)
-                          <td class="text-center py-4 px-6">MF1</td>
-                          @break
-                      @case(6)
-                          <td class="text-center py-4 px-6">MF1</td>
-                          @break
+                                @case($item->UTI_NIVEAU > 0 && $item->UTI_NIVEAU < 5)
+                                    <td class="text-center py-4 px-6">N{{ $item->UTI_NIVEAU }}</td>
+                                @break
 
-                  @endswitch
+                                @case(5)
+                                    <td class="text-center py-4 px-6">MF1</td>
+                                @break
 
-                  <td class="text-center py-4 px-6">{{$item->UTI_MAIL}}</td>
+                                @case(6)
+                                    <td class="text-center py-4 px-6">MF1</td>
+                                @break
+                            @endswitch
 
-              </tr>
+                            <td class="text-center py-4 px-6">{{ $item->UTI_MAIL }}</td>
 
-            @endif
-        @endforeach
+                            </tr>
+                        @endif
+                    @endforeach
 
-      </tbody>
-    </table>
-</div>
+                </tbody>
+            </table>
+        </div>
 
-<h1 class="font-bold text-[#004B85] text-center text-2xl m-4">Liste des initiateurs</h1>
-<div class="w-3/4 max-w-5xl scalable overflow-y-scroll mb-16 max-h-96 bg-white shadow-lg rounded-lg flex items-center justify-center mx-auto">
-    <table class="min-w-full table-auto">
-        <thead>
-          <tr class="bg-gray-200">
-            <th class="text-center py-4 px-6">NOM</th>
-            <th class="text-center py-4 px-6">PRENOM</th>
-            <th class="text-center py-4 px-6">NIVEAU</th>
-            <th class="text-center py-4 px-6">MAIL</th>
-          </tr>
-        </thead>
-        <tbody>
-           @foreach ($initiatorList as $item)
-            @if($item->UTI_SUPPRIME==0)
+        <h1 class="font-bold text-[#ffffff] text-center text-2xl m-4">Liste des initiateurs</h1>
+        <div
+            class="w-3/4 max-w-5xl scalable overflow-y-scroll mb-16 max-h-96 bg-white shadow-lg rounded-lg flex items-center justify-center mx-auto">
+            <table class="min-w-full table-auto">
+                <thead>
+                    <tr class="bg-gray-200">
+                        <th class="text-center py-4 px-6">NOM</th>
+                        <th class="text-center py-4 px-6">PRENOM</th>
+                        <th class="text-center py-4 px-6">NIVEAU</th>
+                        <th class="text-center py-4 px-6">MAIL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($initiatorList as $item)
+                        @if ($item->UTI_SUPPRIME == 0)
+                            <td class="text-center py-4 px-6">{{ $item->UTI_NOM }}</td>
+                            <td class="text-center py-4 px-6">{{ $item->UTI_PRENOM }}</td>
+                            @switch($item->UTI_NIVEAU)
+                                @case(0)
+                                    <td class="text-center py-4 px-6">PAS DE NIVEAU</td>
+                                @break
 
-                    <td class="text-center py-4 px-6">{{$item->UTI_NOM}}</td>
-                    <td class="text-center py-4 px-6">{{$item->UTI_PRENOM}}</td>
-                    @switch($item->UTI_NIVEAU)
-                        @case(0)
-                            <td class="text-center py-4 px-6">PAS DE NIVEAU</td>
-                            @break
-                        @case($item->UTI_NIVEAU > 0 && $item->UTI_NIVEAU < 5 )
-                            <td class="text-center py-4 px-6">N{{$item->UTI_NIVEAU}}</td>
-                            @break
-                        @case(5)
-                            <td class="text-center py-4 px-6">MF1</td>
-                            @break
-                        @case(6)
-                            <td class="text-center py-4 px-6">MF1</td>
-                            @break
+                                @case($item->UTI_NIVEAU > 0 && $item->UTI_NIVEAU < 5)
+                                    <td class="text-center py-4 px-6">N{{ $item->UTI_NIVEAU }}</td>
+                                @break
 
-                    @endswitch
+                                @case(5)
+                                    <td class="text-center py-4 px-6">MF1</td>
+                                @break
 
-                    <td class="text-center py-4 px-6">{{$item->UTI_MAIL}}</td>
+                                @case(6)
+                                    <td class="text-center py-4 px-6">MF1</td>
+                                @break
+                            @endswitch
 
-                </tr>
+                            <td class="text-center py-4 px-6">{{ $item->UTI_MAIL }}</td>
 
-              @endif
-          @endforeach
+                            </tr>
+                        @endif
+                    @endforeach
 
-        </tbody>
-      </table>
-  </div>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    @include('footer')
 
-@endsection
+
 
 
 </body>
--- resources/views/recapitulatif.blade.php
+++ resources/views/recapitulatif.blade.php
@@ -11,88 +11,92 @@
 <body class="bg-gray-50">
 
     @include('header')
+    <div class="bg-[url('/img/underwater.png')] h-full bg-cover bg-no-repeat bg-center p-10 justify-items-center">
+        <div class="w-full p-6 bg-white border border-gray-300 rounded-lg shadow-md sm:p-8 my-10 overflow-x-auto">
 
-    <div class="w-full p-6 bg-white border border-gray-300 rounded-lg shadow-md sm:p-8 my-10 overflow-x-auto">
 
-
-        <form action="{{ route('seance-store') }}" method="POST">
-
-            <input type="hidden" name="SEA_ID" value="{{ $seance->SEA_ID }}">
-
-            <table class="table-auto border-collapse mb-6 w-full">
+            <form action="{{ route('seance-store') }}" method="POST">
 
                 <input type="hidden" name="SEA_ID" value="{{ $seance->SEA_ID }}">
 
                 <table class="table-auto border-collapse mb-6 w-full">
-                    <thead>
-                        <tr>
-                            <th class="px-4 py-2 text-left font-medium text-gray-600 bg-gray-100">Nom de l'élève</th>
-                            <th class="px-4 py-2 text-left font-medium text-gray-600 bg-gray-100">Présence</th>
-                            <th class="px-4 py-2 text-left font-medium text-gray-600 bg-gray-100">Aptitude à Évaluer
-                            </th>
-                            <th class="px-4 py-2 text-left font-medium text-gray-600 bg-gray-100">Évaluation</th>
-                            <th class="px-4 py-2 text-left font-medium text-gray-600 bg-gray-100">Commentaire</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($eleves as $eleve)
-                            @php
-                                $isInitiateur =
-                                    $currentUser->UTI_ID === $eleve->getInitiator($seance->SEA_ID)[0]->user->UTI_ID;
-                            @endphp
-                            <tr class="border-b hover:bg-gray-50">
-                                <td class="px-4 py-2">{{ $eleve->user->UTI_NOM }}</td>
-                                <td class="px-4 py-2">
-                                    <input type="checkbox" name="presence[{{ $eleve->user->UTI_ID }}]"
-                                        value="{{ $eleve->user->UTI_ID }}" {{ $isInitiateur ? '' : 'disabled' }} checked
-                                        class="form-checkbox">
-                                </td>
-                                <td class="px-4 py-2">
-                                    @foreach ($seance->getAptEleve($eleve->user->UTI_ID) as $apt)
-                                        <div class="text-sm text-gray-600">
-                                            <strong>{{ $apt->APT_LIBELLE }}</strong>
-                                        </div>
-                                    @endforeach
-                                </td>
-                                <td class="px-4 py-2">
-                                    @foreach ($seance->getAptEleve($eleve->user->UTI_ID) as $apt)
-                                        <?php
-                                        $evaluationValue = isset($default[$eleve->user->UTI_ID][$apt->APT_CODE]) ? $default[$eleve->user->UTI_ID][$apt->APT_CODE]['evaluation'] : null;
-                                        ?>
-                                        <select name="evaluation[{{ $eleve->user->UTI_ID }}][{{ $apt->APT_CODE }}]"
-                                            {{ $isInitiateur ? '' : 'disabled' }}
-                                            class="form-select mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
-                                            <option value="Non évaluée"
-                                                {{ $evaluationValue === 'Non évaluée' ? 'selected' : '' }}>Non évaluée
-                                            </option>
-                                            <option value="En cours"
-                                                {{ $evaluationValue === 'En cours' ? 'selected' : '' }}>En cours
-                                            </option>
-                                            <option value="Acquis"
-                                                {{ $evaluationValue === 'Acquis' ? 'selected' : '' }}>Acquis</option>
-                                        </select>
-                                    @endforeach
-                                </td>
-                                <td class="px-4 py-2">
-                                    @foreach ($seance->getAptEleve($eleve->user->UTI_ID) as $apt)
-                                        <div>
-                                            <textarea name="commentaire[{{ $eleve->user->UTI_ID }}][{{ $apt->APT_CODE }}]" rows="2"
-                                                placeholder="Commentaire..." {{ $isInitiateur ? '' : 'disabled' }}
-                                                class="form-textarea mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                                                style="min-height: 50px;">{{ isset($default[$eleve->user->UTI_ID][$apt->APT_CODE]) ? $default[$eleve->user->UTI_ID][$apt->APT_CODE]['commentaire'] : '' }}</textarea>
-                                        </div>
-                                    @endforeach
-                                </td>
+
+                    <input type="hidden" name="SEA_ID" value="{{ $seance->SEA_ID }}">
+
+                    <table class="table-auto border-collapse mb-6 w-full">
+                        <thead>
+                            <tr>
+                                <th class="px-4 py-2 text-left font-medium text-gray-600 bg-gray-100">Nom de l'élève
+                                </th>
+                                <th class="px-4 py-2 text-left font-medium text-gray-600 bg-gray-100">Présence</th>
+                                <th class="px-4 py-2 text-left font-medium text-gray-600 bg-gray-100">Aptitude à Évaluer
+                                </th>
+                                <th class="px-4 py-2 text-left font-medium text-gray-600 bg-gray-100">Évaluation</th>
+                                <th class="px-4 py-2 text-left font-medium text-gray-600 bg-gray-100">Commentaire</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($eleves as $eleve)
+                                @php
+                                    $isInitiateur =
+                                        $currentUser->UTI_ID === $eleve->getInitiator($seance->SEA_ID)[0]->user->UTI_ID;
+                                @endphp
+                                <tr class="border-b hover:bg-gray-50">
+                                    <td class="px-4 py-2">{{ $eleve->user->UTI_NOM }}</td>
+                                    <td class="px-4 py-2">
+                                        <input type="checkbox" name="presence[{{ $eleve->user->UTI_ID }}]"
+                                            value="{{ $eleve->user->UTI_ID }}" {{ $isInitiateur ? '' : 'disabled' }}
+                                            checked class="form-checkbox">
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        @foreach ($seance->getAptEleve($eleve->user->UTI_ID) as $apt)
+                                            <div class="text-sm text-gray-600">
+                                                <strong>{{ $apt->APT_LIBELLE }}</strong>
+                                            </div>
+                                        @endforeach
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        @foreach ($seance->getAptEleve($eleve->user->UTI_ID) as $apt)
+                                            <?php
+                                            $evaluationValue = isset($default[$eleve->user->UTI_ID][$apt->APT_CODE]) ? $default[$eleve->user->UTI_ID][$apt->APT_CODE]['evaluation'] : null;
+                                            ?>
+                                            <select name="evaluation[{{ $eleve->user->UTI_ID }}][{{ $apt->APT_CODE }}]"
+                                                {{ $isInitiateur ? '' : 'disabled' }}
+                                                class="form-select mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                                                <option value="Non évaluée"
+                                                    {{ $evaluationValue === 'Non évaluée' ? 'selected' : '' }}>Non
+                                                    évaluée
+                                                </option>
+                                                <option value="En cours"
+                                                    {{ $evaluationValue === 'En cours' ? 'selected' : '' }}>En cours
+                                                </option>
+                                                <option value="Acquis"
+                                                    {{ $evaluationValue === 'Acquis' ? 'selected' : '' }}>Acquis
+                                                </option>
+                                            </select>
+                                        @endforeach
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        @foreach ($seance->getAptEleve($eleve->user->UTI_ID) as $apt)
+                                            <div>
+                                                <textarea name="commentaire[{{ $eleve->user->UTI_ID }}][{{ $apt->APT_CODE }}]" rows="2"
+                                                    placeholder="Commentaire..." {{ $isInitiateur ? '' : 'disabled' }}
+                                                    class="form-textarea mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                                    style="min-height: 50px;">{{ isset($default[$eleve->user->UTI_ID][$apt->APT_CODE]) ? $default[$eleve->user->UTI_ID][$apt->APT_CODE]['commentaire'] : '' }}</textarea>
+                                            </div>
+                                        @endforeach
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
 
-                <button type="submit"
-                    class="px-6 py-3 mt-4 text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm font-semibold">Enregistrer</button>
-        </form>
+                    <button type="submit"
+                        class="px-6 py-3 mt-4 text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm font-semibold">Enregistrer</button>
+            </form>
+        </div>
     </div>
-
+    @include('footer')
 </body>
 
 </html>
